8ae7ec0ab21edb2cacffdbf1e1bd83fe7e53ce17
Mingwei Li <mingweiarthurli@gmail.com>
2020-03-11 16:18:30 -0600

    added simpleJWT

--- api/api/urls.py
+++ api/api/urls.py
@@ -3,6 +3,12 @@ import uuid
 from django.urls import path, include
 from rest_framework import routers
 
+# simple JWT
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 router = routers.DefaultRouter(trailing_slash=True)
 
 
@@ -16,4 +22,8 @@ urlpatterns = [
     #path('api/comments', include('comments.urls')),
     #path('api/friendships/', include('friendships.urls')),
     #path('api-auth/', include('rest_framework.urls')),
+
+    # simple JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
