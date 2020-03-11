f7abc1321ce2f0a3cef6b993a704d08d51bbfe3b
Mingwei Li <mingweiarthurli@gmail.com>
2020-03-11 00:19:32 -0600

    update posts implementation

--- api/posts/serializers.py
+++ api/posts/serializers.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
-        fields = ('title','source', 'origin', 'description', 'contentType', 'content', 'author', 'categories', 'published', 'id', 'visibility', 'unlisted')
+        fields = ('id', 'published', 'title', 'description', 'contentType', 'content', 'categories', 'visibility', 'unlisted')
--- api/posts/urls.py
+++ api/posts/urls.py
@@ -7,5 +7,5 @@ router.register(r'posts', views.PostsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    #path('api-auth/', include('rest_framework.urls', namespace='rest_framework_api'))
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework_api'))
 ]
