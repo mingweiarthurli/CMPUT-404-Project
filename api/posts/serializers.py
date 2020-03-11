0ea8c749c4c8e5b4c3d87dd299e7ff0286935947
Student <defounri@ualberta.ca>
2020-03-11 01:03:16 -0400

    temporarily fixed apis for use with stripped down models

--- api/posts/serializers.py
+++ api/posts/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Post
 
-class PostsSerializer(serializers.HyperlinkedModelSerializer):
+class PostsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('title','source', 'origin', 'description', 'contentType', 'content', 'author', 'categories', 'published', 'id', 'visibility', 'unlisted')
+        #fields = ('title','source', 'origin', 'description', 'contentType', 'content', 'author', 'categories', 'published', 'id', 'visibility', 'unlisted')
+        fields = ('title', 'content', 'user', 'published', 'id')
--- api/users/serializers.py
+++ api/users/serializers.py
@@ -5,5 +5,6 @@ class AuthorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Author
         #Note: id, url and host are not modifiable by authors themselves
-        fields = ['id', 'url', 'host', 'displayName', 'github', 'a_email', 'a_firstname', 'a_lastname', 'a_description', 'a_birthdate']
-        read_only_fields = ['id', 'url', 'approved']
+        #fields = ['id', 'url', 'host', 'displayName', 'github', 'a_email', 'a_firstname', 'a_lastname', 'a_description', 'a_birthdate']
+        #read_only_fields = ['id', 'url', 'approved']
+        fields = ['id', 'approved', 'displayName', 'a_firstname', 'a_lastname']
