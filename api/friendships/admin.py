96931fd8c6e499253f1e8c0e988305e42b616e4e
Mingwei Li <mingweiarthurli@gmail.com>
2020-03-11 16:41:31 -0600

    remove api for reimplementaion

--- api/friendships/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Follower, FriendRequest, Friend
-
-@admin.register(Follower, FriendRequest, Friend)
-class FriendshipsAdmin(admin.ModelAdmin):
-    pass
-#admin.site.register(Followers,FriendRequests,Friends)
-# Register your models here.
--- api/posts/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-
-class PostsSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Post
-        fields = ('id', 'published', 'title', 'description', 'contentType', 'content', 'categories', 'visibility', 'unlisted')
