7847927aebc155eb4f8aa4287eff032034704a7f
Dasha-Kinsely <dashakinsely@gmail.com>
2020-03-12 23:45:43 -0600

    frontend not has perfect connection to backend

--- frontend/src/Layouts/SpiltContainer.js
+++ frontend/src/Layouts/SpiltContainer.js
@@ -1,64 +1,119 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid, List } from "semantic-ui-react";
+import { Container, Grid, List, Button, Icon, Image } from "semantic-ui-react";
 import axios from "axios";
 
 const SplitContainer = () => {
-  const [data, setData] = useState({ hits: [] });
-  const [query, setQuery] = useState("redux");
-  const [url, setUrl] = useState(
-    "https://hn.algolia.com/api/v1/search?query=redux"
-  );
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
+  const [authorData, setAuthorData] = useState([]);
+  const [postData, setPostData] = useState([]);
+  const [authorLoading, setAuthorLoading] = useState(false);
+  const [authorError, setAuthorError] = useState(false);
+  const [postError, setPostError] = useState(false);
+  const [postLoading, setPostLoading] = useState(false);
   useEffect(() => {
-    const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
+    const getAuthors = async () => {
+      setAuthorError(false);
+      setAuthorLoading(true);
       try {
-        const result = await axios(url); //get data from api's url
-        setData(result.data); //
+        const authorFetcher = await axios("http://localhost:8000/api/authors/"); //get data from api's url
+        setAuthorData(authorFetcher.data);
       } catch (error) {
-        setIsError(true);
+        setAuthorError(true);
       }
-      setIsLoading(false);
+      setAuthorLoading(false);
     };
-
-    fetchData();
-  }, [url]);
-
+    const getPosts = async () => {
+      setPostError(false);
+      setPostLoading(true);
+      try {
+        const postFetcher = await axios("http://localhost:8000/api/posts/");
+        setPostData(postFetcher.data);
+      } catch (error) {
+        setPostError(true);
+      }
+      setPostLoading(false);
+    };
+    getPosts();
+    getAuthors();
+  }, []);
+  const openContent = (e, content) => {
+    e.preventDefault();
+    console.log(content);
+  };
   return (
     <Container textAlign="center">
       <Grid column={2} divided>
         <Grid.Row>
-          <Grid.Column>
-            <input
-              type="text"
-              value={query}
-              onChange={event => setQuery(event.target.value)}
-            />
-            <button
-              type="button"
-              onClick={() =>
-                setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
-              }
-            >
-              Search
-            </button>
-            {isError && <div>Something went wrong ...</div>}
-            {isLoading ? (
-              <div>Loading ...</div>
+          <Grid.Column width={3}>
+            {authorError && (
+              <List.Item>
+                <List.Content>
+                  <List.Description>Something went wrong ...</List.Description>
+                </List.Content>
+              </List.Item>
+            )}
+            {authorLoading ? (
+              <List.Item>
+                <List.Content>
+                  <List.Description>Loading ...</List.Description>
+                </List.Content>
+              </List.Item>
             ) : (
-              <ul>
-                {data.hits.map(item => (
-                  <li key={item.objectID}>
-                    <a href={item.url}>{item.title}</a>
-                  </li>
+              <List divided relaxed>
+                {authorData.map(item => (
+                  <List.Item key={item.id}>
+                    <Image
+                      avatar
+                      src="https://react.semantic-ui.com/images/avatar/small/rachel.png"
+                    />
+                    <List.Content>
+                      <List.Header>{item.displayName}</List.Header>
+                      <Button animated size="tiny">
+                        <Button.Content visible>Follow</Button.Content>
+                        <Button.Content hidden>
+                          <Icon name="arrow right" />
+                        </Button.Content>
+                      </Button>
+                    </List.Content>
+                  </List.Item>
                 ))}
-              </ul>
+              </List>
             )}
           </Grid.Column>
-          <Grid.Column></Grid.Column>
+          <Grid.Column width={13}>
+            {postError && (
+              <List.Item>
+                <List.Content>
+                  <List.Description>Something went wrong ...</List.Description>
+                </List.Content>
+              </List.Item>
+            )}
+            {postLoading ? (
+              <List.Item>
+                <List.Content>
+                  <List.Description>Loading ...</List.Description>
+                </List.Content>
+              </List.Item>
+            ) : (
+              <List divided relaxed>
+                {postData.map(item => (
+                  <List.Item key={item.id}>
+                    <List.Content>
+                      <List.Header
+                        as="a"
+                        onClick={e => {
+                          openContent(e, item.content);
+                        }}
+                      >
+                        {item.title}
+                      </List.Header>
+                      <List.Description>{item.description}</List.Description>
+                      <List.Description>{item.published}</List.Description>
+                    </List.Content>
+                  </List.Item>
+                ))}
+              </List>
+            )}
+          </Grid.Column>
         </Grid.Row>
       </Grid>
     </Container>
