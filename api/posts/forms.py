316e1c654c0e92ec2502cb537ef66c25dbd42b79
Mingwei Li <mingweiarthurli@gmail.com>
2020-03-13 17:31:05 -0600

    remove api folder (merged an wrong branch)

--- api/posts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Post
-
-class NewPostForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.Textarea(), max_length=2000)
-
-    class Meta:
-        model = Post
-        fields = ['title', 'content']
--- api/posts/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-
-class PostsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        #fields = ('title','source', 'origin', 'description', 'contentType', 'content', 'author', 'categories', 'published', 'id', 'visibility', 'unlisted')
-        fields = ('title', 'content', 'user', 'published', 'id')
