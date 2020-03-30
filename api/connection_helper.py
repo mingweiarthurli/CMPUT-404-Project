87e5c67d94a1e8b4520ef971f3e62461178547ca
Mingwei Li <mingweiarthurli@gmail.com>
2020-03-30 05:55:13 -0600

    partially update host connectivity

--- api/connection_helper.py
+++ api/connection_helper.py
@@ -1,5 +1,8 @@
 import requests
+import re
 from collections import OrderedDict
+
+server_list = ["https://spongebook-develop.herokuapp.com/", ]
 
 def remote_login(host, username, password):
     r = requests.post(host, verify=False, json={"email": username, "password": password})
@@ -12,7 +15,7 @@ def get_remote_users():
     pass
 
 def get_remote_posts(host):
-    r = requests.get(host + "/author/posts", verify=False)
+    r = requests.get(host + "author/posts", verify=False)
     if r.status_code == 200:
         content = r.json()
         num_posts = content["count"]
@@ -41,10 +44,20 @@ def check_friendship(host, userid_local, userid_remote):
     r = requests.get(host + "/author/" + userid_local + "/friends/" + userid_remote, verify=False)
     return r.json()["friends"]
 
+def check_friend(user_url1, user_url2):
+    user_id1 = re.findall(r"(https?://[-A-Za-z0-9+&@#%?=~_|!:,.;]+/)author/([-A-Za-z0-9]+)/?", user_url1, re.I)[0][1]
+    user_id2 = re.findall(r"(https?://[-A-Za-z0-9+&@#%?=~_|!:,.;]+/)author/([-A-Za-z0-9]+)/?", user_url2, re.I)[0][1]
+    for server in server_list:
+        query = f"{server}author/{user_id1}/friends/{user_id2}"
+        r = requests.get(query, verify=False)
+        if r.json()["friends"] == "true":
+            return True
+    return False
+
 def check_FOAF():
     pass
 
-# def main():
+def main():
 #     # print(remote_login("http://127.0.0.1:8000/login/", "test1@gmail.com", "passqwer"))
 
 #     #  post
@@ -54,4 +67,7 @@ def check_FOAF():
 #     # friend2friend
 #     r = check_friendship("https://spongebook-develop.herokuapp.com", "13930e65-ca56-4743-ab8f-2dd6b45e2cb1", "49784bac-7e1d-43f4-a77d-761141c81f66")
 #     print(r)
-# main()
+    a = check_friend("http://127.0.0.1:5454/author/ae345d54-75b4-431b-adb2-fb6b9e547891", "http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e546013")
+    print(a)
+
+main()
